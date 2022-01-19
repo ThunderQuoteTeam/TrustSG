76e19f31fe611f54b9c9c6635525555d8d73ecc2
Joel <joeltwh1999@gmail.com>
2022-01-20 02:41:50 +0800

    update endpoint

--- src/composables/useAxios.js
+++ src/composables/useAxios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default function useAxios() {
     return axios.create({
-        baseURL: `/api`
+        baseURL: `${process.env.AZURE_FUNCTION_APP_URL}/api`
     });
 }
