feec0f459cb6f3b83daa2768678284b7a5c9ad2c
Joel <joeltwh1999@gmail.com>
2022-01-20 11:45:04 +0800

    env var

--- quasar.conf.js
+++ quasar.conf.js
@@ -45,7 +45,10 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      env: require('dotenv').config().parsed,
+      // env: require('dotenv').config().parsed,
+      env: {
+        AZURE_FUNCTION_APP_URL: "https://trustsg-backend.azurewebsites.net"
+      },
 
       // transpile: false,
       // publicPath: '/',
